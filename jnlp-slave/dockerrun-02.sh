62f647f64880382ba6c6d29badcb68dbf5be2b63
hotpeperoncino <bluenemophila@gmail.com>
2018-02-12 16:59:06 +0900

    arrange dockerrun.sh

--- jnlp-slave/dockerrun-02.sh
+++ jnlp-slave/dockerrun-02.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-# change URL
-export URL=http://192.168.116.207:8880/computer/2/slave-agent.jnlp
 
-SLAVEDIR=~/ci/slaves/3
-SLAVEPORT=12022
-
-mkdir -p $SLAVEDIR
-
-docker run \
-    --name "ci-jnlp-slave" \
-    -p $SLAVEPORT:22 \
-    -v $SLAVEDIR:/var/lib/jenkins \
-    -e URL=$URL \
-    -d \
-    ci/jnlp-slave
-
+bash -x ./dockerrun.sh 02  \
+   http://192.168.116.207:8880/computer/02/slave-agent.jnlp \
+   12222 \
+   ~/.ci/slaves
 
--- jnlp-slave/dockerrun.sh
+++ jnlp-slave/dockerrun.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+#
+# 1 - node id
+# 2 - jnlp url
+# 3 - ssh port
+# 4 - slave workspace
+#
+
+
 # change URL
 export NODEID=$1
 export URL=$2
 export PORT=$3
+export WS=$4
 
-SLAVEDIR=~/ci/slaves/$NODEID
+SLAVEDIR=$WS/$NODEID
 SLAVESSHPORT=$PORT
 
 mkdir -p $SLAVEDIR
 
 docker run \
-    --name "ci-jnlp-slave" \
+    --name "ci-jnlp-slave-"$NODEID \
     -p $SLAVESSHPORT:22 \
     -v $SLAVEDIR:/var/lib/jenkins \
     -e URL=$URL \
